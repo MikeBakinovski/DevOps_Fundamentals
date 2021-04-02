c53b2737f90dfd7655176530742a78f079085293
MikeBakinovski <mike.bakinovski@gmail.com>
2021-04-03 02:09:31 +0300

    readmemd

---
+++ 03 Scripting PowerShell Essentials/NewFolders.ps1
@@ -0,0 +1,14 @@
+﻿Get-Content -Path 'F:\00_EPAM\GIT\DevOps_Fundamentals\03 Scripting PowerShell Essentials\Dir_Names.txt' | 
+
+foreach{
+$name = "F:\00_EPAM\GIT\DevOps_Fundamentals\03 Scripting PowerShell Essentials\$_" 
+New-Item -Path $name -ItemType Directory
+
+$filename = "$name\README.md"
+New-Item -Path $filename -ItemType File
+
+$name = "F:\00_EPAM\GIT\DevOps_Fundamentals\03 Scripting PowerShell Essentials\$_\Images"
+New-Item -Path $name -ItemType Directory
+$name = "F:\00_EPAM\GIT\DevOps_Fundamentals\03 Scripting PowerShell Essentials\$_\Scripts"
+New-Item -Path $name -ItemType Directory
+}
