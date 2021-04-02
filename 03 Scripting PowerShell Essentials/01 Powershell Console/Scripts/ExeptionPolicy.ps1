c57209b1f180e5d7d4fa9f402ee38ecee67fe774
MikeBakinovski <mike.bakinovski@gmail.com>
2021-04-03 02:14:04 +0300

    Script

---
+++ 03 Scripting PowerShell Essentials/01 Powershell Console/Scripts/ExeptionPolicy.ps1
@@ -0,0 +1,5 @@
+﻿clear
+#Set-ExecutionPolicy -ExecutionPolicy RemoteSigned -Scope LocalMachine
+Set-ExecutionPolicy -ExecutionPolicy Restricted -Scope LocalMachine
+Get-ExecutionPolicy
+# PowerShell.exe -ExecutionPolicy Bypass -File .runme.ps1
